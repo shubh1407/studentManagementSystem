5348a718c8a2c1ca8f91b0fba3a5484b0f942832
shubh1407 <shubhamagarwal1407@gmail.com>
2022-06-16 14:52:06 +0530

    treeset implementation

--- src/com/studentManagement/storage/StudentManagementStorage.java
+++ src/com/studentManagement/storage/StudentManagementStorage.java
@@ -1,7 +1,9 @@
 package com.studentManagement.storage;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 import com.studentManagement.bean.StudentRecord;
 
@@ -10,4 +12,11 @@ public class StudentManagementStorage {
 	public static ArrayList<StudentRecord> studentRecords = new ArrayList<StudentRecord>();
 	
 	public static LinkedHashSet<StudentRecord> studentRecordSet = new LinkedHashSet<StudentRecord>();
+	
+	public static TreeSet<StudentRecord> studentRecordTreeSet = new TreeSet<StudentRecord>(new Comparator<StudentRecord>() {
+		@Override
+		public int compare(StudentRecord o1, StudentRecord o2) {
+			return o1.getRollNo()-o2.getRollNo();
+		};
+	});
 }
