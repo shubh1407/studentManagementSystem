7364ac048ba6d8c84630720b08076cfdaf24debb
shubh1407 <shubhamagarwal1407@gmail.com>
2022-06-17 09:54:02 +0530

    implementing hashmap

--- src/com/studentManagement/storage/StudentManagementStorage.java
+++ src/com/studentManagement/storage/StudentManagementStorage.java
@@ -2,6 +2,7 @@ package com.studentManagement.storage;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.TreeSet;
 
@@ -19,4 +20,6 @@ public class StudentManagementStorage {
 			return o1.getRollNo()-o2.getRollNo();
 		};
 	});
+	
+	public static HashMap<Integer, StudentRecord> studentRecordMap = new HashMap<Integer, StudentRecord>();
 }
